adab5aa40a7070f4eb40e15134841b1116fb02de
triad7 <munpinoy@gmail.com>
2023-07-11 10:27:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import { MyContext } from './context';
+import Stage1 from './components/Stage_1';
+import Stage2 from './components/Stage_2';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  './App.css';
+
+class App extends Component {
+
+    static contextType = MyContext;
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="center-wrapper">
+          <h1>Who pays the bill?</h1>
+          { this.context.state.stage === 1 ?
+          <Stage1 />
+            :
+            <Stage2 /> }
+        </div>
+      </div>
+    )
+  }
+}
+export default App;
